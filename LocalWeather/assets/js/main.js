8755bdfee4de1c2cecc8ccc1ee556b8743fbf6ac
manujas <arias.emanuel@gmail.com>
2016-08-21 02:44:43 -0300

    adding DOM update

--- LocalWeather/assets/js/main.js
+++ LocalWeather/assets/js/main.js
@@ -1,8 +1,29 @@
 // Whole-script strict mode syntax
 "use strict";
 
+var weatherIcon = {
+    "01d": "wi-day-sunny",
+    "01n": "wi-night-clear",
+    "02d": "wi-day-cloudy",
+    "02n": "wi-night-alt-cloudy",
+    "03d": "wi-cloud",
+    "03n": "wi-cloud",
+    "04d": "wi-cloudy",
+    "04n": "wi-cloudy",
+    "09d": "wi-rain",
+    "09n": "wi-rain",
+    "10d": "wi-day-rain",
+    "10n": "wi-night-alt-rain",
+    "11d": "wi-thunderstorm",
+    "11n": "wi-thunderstorm",
+    "13d": "wi-snow",
+    "13n": "wi-snow",
+    "50d": "wi-fog",
+    "50n": "wi-fog"
+};
+
 /**
- *
+ * Get weather data by coors
  */
 function getWeatherData(coords, callback) {
     var weatherData;
@@ -20,14 +41,34 @@ function getWeatherData(coords, callback) {
     });
 }
 
+function setTemp(temp) {
+    temp = Math.ceil(temp);
+    $("#w-temp").text(temp + "°");
+}
+
+function setType(type) {
+    type = type.charAt(0).toUpperCase() + type.slice(1);
+    $("#w-type").text(type);
+}
+
+function setCity(city) {
+    $("#w-city").text(city);
+}
+
+function setIcon(icon) {
+    var iconClass = "wi " + weatherIcon[icon];
+    $("#w-icon").removeClass().addClass(iconClass);
+}
 /**
- *
+ * Update de DOM with the weather data
  */
 function showWeather(weatherData) {
-    $("#w-temp").text(Math.ceil(weatherData.main.temp) + "°");
-    $("#w-type").text(weatherData.weather[0].description);
-    $("#w-city").text(weatherData.name);
+    setTemp(weatherData.main.temp);
+    setType(weatherData.weather[0].description);
+    setCity(weatherData.name);
+    setIcon(weatherData.weather[0].icon);
 }
+
 
 // the magic start here
 (function(){
@@ -42,10 +83,10 @@ function showWeather(weatherData) {
             lon: position.coords.latitude,
             log: position.coords.longitude
         };
+
+        // get weather data & update the DOM
         getWeatherData(coords, showWeather);
-        // get weather data
-        // set icon, city and type of weather
+
         // change background gradient by the hours
     });
-
 })();
