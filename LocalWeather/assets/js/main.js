4ac45196357bc3c7f0000e334405ed3131bdeddb
manujas <arias.emanuel@gmail.com>
2016-08-21 15:33:25 -0300

    LW - adding error handeling

--- LocalWeather/assets/js/main.js
+++ LocalWeather/assets/js/main.js
@@ -1,6 +1,7 @@
 // Whole-script strict mode syntax
 "use strict";
 
+// icon dictionary
 var weatherIcon = {
     "01d": "wi-day-sunny",
     "01n": "wi-night-clear",
@@ -37,28 +38,43 @@ function getWeatherData(coords, callback) {
         callback(data);
     })
     .fail(function(err) {
-        console.log(err.responseText);
+        console.log(err.responceText);
+        var errMsg = "Sorry, our meteorologist is taking a snap. Try again";
+        handleError(true, errMsg);
     });
 }
 
+/**
+ * DOM manipulation for temp
+ */
 function setTemp(temp) {
     temp = Math.ceil(temp);
     $("#w-temp").text(temp + "°");
 }
 
+/**
+ * DOM manipulation for type of weather
+ */
 function setType(type) {
     type = type.charAt(0).toUpperCase() + type.slice(1);
     $("#w-type").text(type);
 }
 
+/**
+ * DOM manipulation for city
+ */
 function setCity(city) {
     $("#w-city").text(city);
 }
 
+/**
+ * DOM manipulation for icon
+ */
 function setIcon(icon) {
     var iconClass = "wi " + weatherIcon[icon];
     $("#w-icon").removeClass().addClass(iconClass);
 }
+
 /**
  * Update de DOM with the weather data
  */
@@ -69,13 +85,43 @@ function showWeather(weatherData) {
     setIcon(weatherData.weather[0].icon);
 }
 
+/**
+ * Shown/hide error function
+ */
+function handleError(show, msg) {
+    msg = msg || "Sorry, we don't now what happen. Try again";
+    var errBox = $("#err-box");
+    var weatherBox = $("#weather-box");
+
+    // hide all boxes
+    if (!errBox.hasClass('hide')) {
+        errBox.addClass("hide", 1500);
+    }
+    if (!weatherBox.hasClass('hide')) {
+        weatherBox.addClass("hide", 1500);
+    }
+
+    // show the right box
+    if (show) {
+        setTimeout(function() {
+            errBox.html(msg).removeClass("hide", 1500);
+        }, 1500);
+        return;
+    } else {
+        setTimeout(function() {
+            weatherBox.removeClass("hide");
+        }, 1500);
+    }
+}
 
 // the magic start here
 (function(){
     // Loader
-    // get coordinates
+
+    // check browser support
     if (!navigator.geolocation) {
-        // Here goes error handeling
+        handleError(true, "Sorry, your browser has not support for geolocation. Try with Firefox or Chrome");
+        return;
     }
 
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -88,5 +134,16 @@ function showWeather(weatherData) {
         getWeatherData(coords, showWeather);
 
         // change background gradient by the hours
+
+    }, function(posErr) { // on error
+        // default msg
+        var errMsg = "Sorry, something it's not right. Try again.";
+
+        // user block
+        if (posErr.code === 1) {
+            errMsg = "Please, we need that you enable geolocation in your browser.";
+        }
+
+        handleError(true, errMsg);
     });
 })();
