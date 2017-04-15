89dd65aa929572e1c6e054c82142eb661c47b5f5
Emanuel Arias <arias.emanuel@gmail.com>
2017-04-15 09:54:40 -0300

    WV - cleaning up js (again) for better code reading

--- WikipediaViewer/assets/js/main.js
+++ WikipediaViewer/assets/js/main.js
@@ -1,62 +1,103 @@
-$("#wiki_search").keyup(function (e) {
-  if (e.keyCode == 13) {
-    var searchForm = $(".search-form");
-    if (searchForm.hasClass("full-height")) {
-      searchForm.removeClass("full-height")
-    }
-    var searchResults = $(".search-results").children();
-    if (searchResults.children()) {
-      searchResults.html('');
-    }
-
-    searchOnWikipedia($(this).val());
+$('#wiki_search').keyup( function(e) {
+  // onlly listen enter key
+  if (e.keyCode !== 13) {
+    return;
   }
+
+  // save search value
+  const searchValue = $(this).val();
+
+  // only serach if the input has char
+  if (searchValue == '') {
+    return;
+  }
+
+  moveSearchFormToTop();
+  cleanSearchResult();
+  searchOnWikipedia(searchValue);
 });
 
-function searchOnWikipedia(query) {
-  var url = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + query;
+/**
+ * Search on wikipedia
+ * @param  String query
+ */
+const searchOnWikipedia = query => {
+  const url = 'https://crossorigin.me/https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + query;
 
   $.ajax( {
     url: url,
     type: 'GET',
-    success: function(data) {
+    success: data => {
       showResults(data.query.pages);
     }
   });
-}
+};
 
-function showResults(results) {
-  for(var id in results) {
+/**
+ * Populate the DOM with the result from wikipedia
+ * @param  Json results
+ */
+const showResults = results => {
+  for(const id in results) {
     insertNewHtmlCard(results[id]);
   }
   toggleCardsAnimation();
-}
+};
 
-function insertNewHtmlCard(result) {
-  var wikiURL = "https://en.wikipedia.org/?curid=";
-  var card = "<div class='col s12 m6 card-col'> \
-                <div class='card hidden'> \
-                  <div class='card-content'> \
-                    <span class='card-title'><a href='' target='_blank'></a></span> \
-                    <p class='card-extract'></p> \
+/**
+ * Creates a result's card
+ * @param  Json result
+ */
+const insertNewHtmlCard = result => {
+  const wikiURL = 'https://en.wikipedia.org/?curid=';
+  const card = '<div class="col s12 m12 card-col"> \
+                <div class="card hidden"> \
+                  <div class="card-content"> \
+                    <span class="card-title"><a href="" target="_blank"></a></span> \
+                    <p class="card-extract"></p> \
                   </div> \
                 </div> \
-              </div>";
-  $(".search-results").children().append(card);
-  $(".card-title").last().children().html(result.title).attr("href", wikiURL+result.pageid);
-  $(".card-extract").last().html(result.extract.truncate(50));
-}
+              </div>';
+  $('.search-results').children().append(card);
+  $('.card-title').last().children().html(result.title).attr('href', wikiURL+result.pageid);
+  $('.card-extract').last().html(result.extract.truncate(150));
+};
 
-function toggleCardsAnimation(eraseCard) {
-  $(".card").each(function(i, card) {
-    setTimeout(function() {
+/**
+ * Toggle animation for a card
+ * @param  card
+ */
+const toggleCardsAnimation = card => {
+  $('.card').each((i, card) => {
+    setTimeout(() => {
       $(card).toggleClass('hidden');
-    }, 250);
+    }, 250 * i);
   });
-}
+};
 
+/**
+ * Animate the serch form from original center position to a top position
+ */
+const moveSearchFormToTop = () => {
+  const searchForm = $('.search-form');
+  if (searchForm.hasClass('full-height')) {
+    searchForm.removeClass('full-height');
+  }
+};
+
+/**
+ * Remove all result's card from the DOM
+ */
+const cleanSearchResult = () => {
+  const searchResults = $('.search-results').children();
+  if (searchResults.children()) {
+    searchResults.html('');
+  }
+};
+
+// added a truncate static method to String prototype
 String.prototype.truncate = function(len) {
   return (this.length > len)
-    ? this.substring(0, len) + "..."
+    ? this.substring(0, len) + '...'
     : this.substring(0, this.length);
 };
